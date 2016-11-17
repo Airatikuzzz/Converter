e38356c5481908221ebdbc14b7657d7c2c8eb141
Airatikuzzz <m.airat1997@gmail.com>
2016-11-18 00:03:50 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace WindowsFormsApplication4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTip2 = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTip3 = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTip4 = new System.Windows.Forms.ToolTip(this.components);
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(29, 131);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(121, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(91, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Выберите единицу измерения";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Бит",
+            "Байт",
+            "КБайт",
+            "МБайт",
+            "ГБайт",
+            "ТБайт"});
+            this.comboBox2.Location = new System.Drawing.Point(208, 94);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 3;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Бит",
+            "Байт",
+            "КБайт",
+            "МБайт",
+            "ГБайт",
+            "ТБайт"});
+            this.comboBox1.Location = new System.Drawing.Point(29, 94);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(208, 131);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(121, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "Двоичная",
+            "Восьмеричная",
+            "Десятичная",
+            "Шестнадцатеричная"});
+            this.comboBox3.Location = new System.Drawing.Point(29, 187);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 21);
+            this.comboBox3.TabIndex = 7;
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(163, 187);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(166, 20);
+            this.textBox3.TabIndex = 8;
+            this.textBox3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox3_KeyPress);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(29, 228);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(76, 17);
+            this.checkBox1.TabIndex = 9;
+            this.checkBox1.Text = "Двоичная";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(29, 251);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(100, 17);
+            this.checkBox2.TabIndex = 10;
+            this.checkBox2.Text = "Восьмеричная";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(29, 274);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(87, 17);
+            this.checkBox3.TabIndex = 11;
+            this.checkBox3.Text = "Десятичная";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBox4.Location = new System.Drawing.Point(29, 297);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(128, 17);
+            this.checkBox4.TabIndex = 12;
+            this.checkBox4.Text = "Шестнадцатеричная";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            this.checkBox4.CheckedChanged += new System.EventHandler(this.checkBox4_CheckedChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("MS Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(163, 226);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(166, 98);
+            this.textBox4.TabIndex = 13;
+            this.textBox4.Text = "  \r\n \r\n \r\n\r\n \r\n \r\n \r\n\r\n \r\n \r\n \r\n \r\n \r\n ";
+            // 
+            // button1
+            // 
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(156, 94);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(46, 57);
+            this.button1.TabIndex = 6;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(359, 354);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.ToolTip toolTip2;
+        private System.Windows.Forms.ToolTip toolTip3;
+        private System.Windows.Forms.ToolTip toolTip4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication4
+{
+    public partial class Form1 : Form
+    {
+        public long b = 1;
+        public double a;
+        public int c = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.SelectedIndex = 0;
+            comboBox2.SelectedIndex = 0;
+            comboBox3.SelectedIndex = 0;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            toolTip1.SetToolTip(comboBox1, "Выберите изначальную величину информации");
+            toolTip2.SetToolTip(comboBox2, "Выберите величину, в которую хотите конвертировать");
+            toolTip3.SetToolTip(button1, "Нажмите для конвертирования");
+            toolTip4.SetToolTip(comboBox3, "Выберите вводимую систему счисления");
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            switch (comboBox2.SelectedIndex)
+            {
+                case 0:
+                    a = Convert.ToDouble(textBox1.Text) * Convert.ToDouble(b);
+                    textBox2.Text = Convert.ToString(a);
+                    break;
+                case 1:
+                    a = Convert.ToDouble(textBox1.Text) * Convert.ToDouble(b) / 8.0;
+                    textBox2.Text = Convert.ToString(a);
+                    break;
+                case 2:
+                    a = Convert.ToDouble(textBox1.Text) * Convert.ToDouble(b) / 8192.0;
+                    textBox2.Text = Convert.ToString(a);
+                    break;
+                case 3:
+                    a = Convert.ToDouble(textBox1.Text) * Convert.ToDouble(b) / 8388608.0;
+                    textBox2.Text = Convert.ToString(a);
+                    break;
+                case 4:
+                    a = Convert.ToDouble(textBox1.Text) * Convert.ToDouble(b) / 8589934592.0;
+                    textBox2.Text = Convert.ToString(a);
+                    break;
+                case 5:
+                    a = Convert.ToDouble(textBox1.Text) * Convert.ToDouble(b) / 8796093022208.0;
+                    textBox2.Text = Convert.ToString(a);
+                    break;
+
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0:
+                    b = 1; break;
+                case 1:
+                    b = 8; break;
+                case 2:
+                    b = 8192; break;
+                case 3:
+                    b = 8388608; break;
+                case 4:
+                    b = 8589934592; break;
+                case 5:
+                    b = 8796093022208; break;
+            }
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (comboBox3.SelectedIndex)
+            {
+                case 0:
+                    c = 2; break;
+                case 1:
+                    c = 8; break;
+                case 2:
+                    c = 10; break;
+                case 3:
+                    c = 16; break;
+            }
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar <= 47 || e.KeyChar >= 58) && e.KeyChar != 8)
+            {
+                e.Handled = true;
+                MessageBox.Show("Вводите только числа!", "Ошибка");
+            }
+        }
+
+        private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            switch (comboBox3.SelectedIndex)
+            {
+                case 0:
+                    if ((e.KeyChar <= 47 || e.KeyChar >= 50) && e.KeyChar != 8)
+                    {
+                        e.Handled = true;
+                        MessageBox.Show("Вводите только двоичные числа. Например : 0101101!", "Ошибка!");
+                    }
+                    break;
+                case 1:
+                    if ((e.KeyChar <= 47 || e.KeyChar >= 56) && e.KeyChar != 8)
+                    {
+                        e.Handled = true;
+                        MessageBox.Show("Вводите только восьмеричные числа. От 0 до 7!", "Ошибка!");
+                    }
+                    break;
+                case 2:
+                    if ((e.KeyChar <= 47 || e.KeyChar >= 58) && e.KeyChar != 8)
+                    {
+                        e.Handled = true;
+                        MessageBox.Show("Вводите только десятичные числа. От 0 до 9!", "Ошибка!");
+                    }
+                    break;
+                case 3:
+                    if ((e.KeyChar <= 47 || e.KeyChar >= 58) && (e.KeyChar <= 64 || e.KeyChar >= 71) && (e.KeyChar <= 96 || e.KeyChar >= 103) && e.KeyChar != 8)
+                    {
+                        e.Handled = true;
+                        MessageBox.Show("Вводите только шестнадцатиричное число. Например : 3D5!", "Ошибка");
+                    }
+                    break;
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                textBox4.Text = textBox4.Text.Insert(textBox4.GetFirstCharIndexFromLine(0), Convert.ToString(Convert.ToInt16(textBox3.Text,c), 2));
+            }
+            else textBox4.Text = textBox4.Text.Remove(textBox4.GetFirstCharIndexFromLine(0), textBox4.Lines[0].Length);
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked == true)
+            {
+                textBox4.Text = textBox4.Text.Insert(textBox4.GetFirstCharIndexFromLine(2), Convert.ToString(Convert.ToInt16(textBox3.Text,c), 8));
+            }
+            else textBox4.Text = textBox4.Text.Remove(textBox4.GetFirstCharIndexFromLine(2), textBox4.Lines[2].Length);
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox3.Checked == true)
+            {
+                textBox4.Text = textBox4.Text.Insert(textBox4.GetFirstCharIndexFromLine(4), Convert.ToString(Convert.ToInt16(textBox3.Text,c), 10));
+            }
+            else textBox4.Text = textBox4.Text.Remove(textBox4.GetFirstCharIndexFromLine(4), textBox4.Lines[4].Length);
+        }
+
+        private void checkBox4_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox4.Checked == true)
+            {
+                textBox4.Text = textBox4.Text.Insert(textBox4.GetFirstCharIndexFromLine(6), Convert.ToString(Convert.ToInt16(textBox3.Text,c), 16));
+            }
+            else textBox4.Text = textBox4.Text.Remove(textBox4.GetFirstCharIndexFromLine(6), textBox4.Lines[6].Length);
+        }
+    }
+}
